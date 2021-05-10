caee53ab00e6f5a9b528eea2dd373db3b40d4047
ihmwhydn <ihm245@gmail.com>
2021-05-10 21:36:44 +0700

    add file

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      autoplay:true,
+      autoplayTimeout:2500,
+      autoplayHoverPause:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+
+  AOS.init();
+
+  });
